c52a293d95d0ac70f3f27413f118d14e493a201e
Noora Sweis <noora.sweis@gmail.com>
2021-02-11 10:16:06 +0200

    NavLink active property

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,13 @@ const Nav = () => {
         <div className="navbar">
             <img className="logo" src="https://i.ibb.co/4F9X66H/logo-1.jpg" alt = "LOGO"/>
 
-            <ul>
-                <Link to="/about">About</Link>
-                <Link to="/">Home</Link>
-                <Link to="/Profile">Profile</Link>
-                <Link to="/Favorates">Favorates</Link>
-                <Link to="/Search">Search</Link>
-                <Link to="/SignIn">SignIn</Link>
+            <ul className="nav-list">
+                <NavLink to="/about">About</NavLink>
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/Profile">Profile</NavLink>
+                <NavLink to="/Favorates">Favorates</NavLink>
+                <NavLink to="/Search">Search</NavLink>
+                <NavLink to="/SignIn">SignIn</NavLink>
             </ul>
         </div>
     );
