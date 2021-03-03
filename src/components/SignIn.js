21ac3976544a0465f2f0871c0f501c9a4b1a6e5a
Noora Sweis <noora.sweis@gmail.com>
2021-03-03 14:57:46 +0200

    sign

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./SignIn.css";
 
-const SignIn = () => {
-    return (
-        <div>
-            <h2>SignIn</h2>
-        </div>
-    );
+export default class SignIn extends Component {
+    render() {
+        return (
+            <div className="split">
+                <div className="left">
+                    <h3>Sign In</h3>
+                </div>
+                <form className="right">
+                    <div className="form-signin">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-signin">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-signin">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Sign in</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </form>
+            </div>
+        );
+    }
 }
-
-export default SignIn;
