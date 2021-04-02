14b4d09117a24fa4f2437e7757d1134ae57ee074
Noora Sweis <noora.sweis@gmail.com>
2021-04-02 20:18:02 +0300

    About us - without CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import './style/About.css';
+import img from './image/team.jpg';
 
 const About = () => {
     return (
-        <div>
-            <h3 className="about">
-            This website aims to help tourists and the people to find the location of the service that they are looking for. In our project, we will focus on the city of Tulkarm. <br></br>
-            <br></br>
-            It includes a map of the city that includes all the services and facilities. The purpose is to identify all the facilities and services of the city. Thus from one hand, we aim to help the user to easily reach his goal. On the other hand, help service providers to add their locations and their products, by including them on the city map. This will help them in making their services or products visible to users. Thus facilitating people's access to the required services through browsing, searching, and selecting the desired location as it is on the map.
-            </h3>
+        <div className='about'>
+            <div className='about-image'>
+                <span>Who We Are? <br /> Engineers, Programmers, Thinkers AND Collaborators</span>
+            </div>
+            <span className='about-paragragh'>This site is designed to showcase Tulkarm area as a services map. It contains many features that enable the user (who is visiting the city or even those residing in it) to find the services they are looking for in an easy way, according to their locations and the services closest to them. It also enables service providers to display their products, services, and locations to make it easier for those looking to find them.
+            </span>
+            <span className='about-team'>Our Team</span>
+
+            <div>
+                <img src={img} alt="team member" />
+                <span>Noora Sweis <br /> CEO - Software Engineer</span>
+            </div>
+            <div>
+                <img src={img} alt="team member" />
+                <span>Leen Odeh <br /> CEO - Software Engineer</span>
+            </div>
+            <div>
+                <img src={img} alt="team member" />
+                <span>Ethar Samahah <br /> CEO - Software Engineer</span>
+            </div>
         </div>
     );
 }
 
 export default About;
+
+/*
+This site is designed to showcase Tulkarm area as a services map.
+It contains many features that enable the user
+(who is visiting the city or even those residing in it)
+to find the services they are looking for in an easy way,
+according to their locations and the services closest to them.
+It also enables service providers
+to display their products, services, and locations
+to make it easier for those looking to find them.
+*/
