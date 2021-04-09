43ce27e0bfbd40a270ddb3db392847be6cdeafec
Noora Sweis <noora.sweis@gmail.com>
2021-04-09 20:05:36 +0300

    close dropdown when clicked

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,60 +1,79 @@
-// JavaScript Document
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./navbar.css";
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { auth } from './config';
+import { NavLink, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { auth } from "./config";
 
 class Navbar extends Component {
-	menu = React.createRef();
 	state = {
 		isLoggedIn: this.props.isLoggedIn,
 		position: this.props.position
-	}
+	};
 
+	componentDidMount() {
+		console.log(document.querySelector("#nav-toggle"));
+		this.props.history.listen((location) => {
+			document.querySelector("#nav-toggle").checked = false;
+		});
+	}
 	logout = (e) => {
 		e.preventDefault();
-		console.log('this.logout')
+		console.log("this.logout");
 		auth.signOut();
-		this.setState({
-			isLoggedIn: false,
-			position: 'client-out'
-		}, function () {
-			console.log(this.state);
-		});
-		window.location.reload(false)
-	}
-
+		this.setState(
+			{
+				isLoggedIn: false,
+				position: "client-out"
+			},
+			function () {
+				console.log(this.state);
+			}
+		);
+		window.location.reload(false);
+	};
 	render() {
 		return (
 			<div className="navbar">
 				<input id="nav-toggle" type="checkbox" />
-				<img className="logo" src="https://i.ibb.co/Dg4r5Gt/logo.png" alt="LOGO" />
+				<img
+					className="logo"
+					src="https://i.ibb.co/Dg4r5Gt/logo.png"
+					alt="LOGO"
+				/>
 
 				<ul className="nav-list">
 					<NavLink exact to="/" className="item">Home</NavLink>
 					<NavLink to="/Profile" className="item">Profile</NavLink>
 					<NavLink to="/Map" className="item">Map</NavLink>
-					{!this.props.isLoggedIn ? <NavLink to="/SignIn" className="item">Sign In</NavLink> : (null)}
-					<NavLink to="/about" className="item">About</NavLink>
+					{!this.props.isLoggedIn ? (
+						<NavLink to="/SignIn" className="item">
+							Sign In
+						</NavLink>
+					) : null}
 
-					{this.props.isLoggedIn ?
-						(<div className="dropdown" id='List' ref={this.menu}>
+					<NavLink to="/about" className="item">
+						About
+          </NavLink>
+					{this.props.isLoggedIn ? (
+						<div className="dropdown" id="List">
 							<button className="item">User Name</button>
-							{this.props.position === 'ADMIN' ?
-								(<div className="dropdown-content">
+
+							{this.props.position === "Admin" ? (
+								<div className="dropdown-content">
 									<NavLink to="/Favorite" className="admin-item">Favorates</NavLink>
-									<NavLink to="/AccountApproval" className="admin-item">Account Approval</NavLink>
+									<NavLink exact to="/AccountApproval" className="admin-item">ccount Approval</NavLink>
 									<NavLink to="/AddPlace" className="admin-item">Add fixed places</NavLink>
 									<NavLink to="/AddNewAdmin" className="admin-item">Add new admin</NavLink>
-									<NavLink onClick={this.logout} exact to="/" className="admin-item">Log Out</NavLink>
-								</div>) : (<div className="dropdown-content">
+									<NavLink onClick={this.logout} exact to="/" className="admin-item" > Log Out</NavLink>
+								</div>
+							) : (
+								<div className="dropdown-content">
 									<NavLink to="/Favorite" className="admin-item">Favorates</NavLink>
-									<NavLink onClick={this.logout} exact to="/" className="admin-item" >Log Out</NavLink>
-								</div>)}
-
-						</div>) : null}
-
+									<NavLink onClick={this.logout} exact to="/" className="admin-item">Log Out</NavLink>
+								</div>
+							)}
+						</div>
+					) : null}
 				</ul>
 				<label htmlFor="nav-toggle" className="icon-burger">
 					<div className="line"></div>
@@ -69,12 +88,12 @@ function mapStateToProps(state) {
 	return {
 		isLoggedIn: state.isLoggedIn,
 		position: state.position
-	}
+	};
 }
 function mapDispatchToProps(dispatch) {
-	console.log('disp')
+	console.log("disp");
 	return {
-		logout: () => dispatch({ type: 'LOGOUT' })
-	}
+		logout: () => dispatch({ type: "LOGOUT" })
+	};
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
