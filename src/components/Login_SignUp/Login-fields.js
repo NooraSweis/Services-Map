6de355df309dc01f5d0193db61c4541e88ba4ff5
Noora Sweis <noora.sweis@gmail.com>
2021-04-16 23:45:07 +0300

    delete account

--- src/components/Login_SignUp/Login-fields.js
+++ src/components/Login_SignUp/Login-fields.js
@@ -3,7 +3,7 @@ import CustomDialogContent from './forget';
 import { CustomDialog } from 'react-st-modal';
 import fire from '../config';
 import { connect } from 'react-redux';
-//import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 
 var checked = false;
@@ -11,41 +11,43 @@ var checked = false;
 class Login_Fields extends Component {
     constructor(props) {
         super(props);
-        this.state={email:'', password:''};
+        this.state = { email: '', password: '' };
         this.login = this.login.bind(this);
-        this.ChangeEmail=this.ChangeEmail.bind(this);
-        this.ChangePass=this.ChangePass.bind(this);
+        this.ChangeEmail = this.ChangeEmail.bind(this);
+        this.ChangePass = this.ChangePass.bind(this);
     }
     login(e) {
         e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(this.state.email.trim(), this.state.password).then((u) => {
-            fire.firestore().collection('User').where('email', '==', this.state.email.trim()).get().then((snapshot) => {
-                snapshot.forEach((doc) => {
-                    this.props.login({ type: 'LOGIN' })
-                    this.props.position({ type: doc.data().type })
-                    console.log("successfully login")
+        fire.auth().setPersistence(checked ? firebase.auth.Auth.Persistence.LOCAL : firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                fire.auth().signInWithEmailAndPassword(this.state.email.trim(), this.state.password).then((u) => {
+                    fire.firestore().collection('User').where('email', '==', this.state.email.trim()).get().then((snapshot) => {
+                        snapshot.forEach((doc) => {
+                            this.props.login({ type: 'LOGIN' })
+                            this.props.position({ type: doc.data().type })
+                            console.log("successfully login")
+                        })
+                    })
+                        .catch((err) => {
+                            console.log('Error: ' + err.toString());
+                        })
+
                 })
+                    .catch((err) => {
+                        console.log('Error: ' + err.toString());
+                    });
             })
-                .catch((err) => {
-                    console.log('Error: ' + err.toString());
-                })
-
-        })
-            .catch((err) => {
-                console.log('Error: ' + err.toString());
-            });
-
     }
 
     handleInputChange(e) {
         checked = !checked;
         console.log(checked)
     }
-    ChangeEmail=(e)=>{
-        this.setState({...this.state,email:e.target.value});
+    ChangeEmail = (e) => {
+        this.setState({ ...this.state, email: e.target.value });
     }
-    ChangePass=(e)=>{
-        this.setState({...this.state,password:e.target.value});
+    ChangePass = (e) => {
+        this.setState({ ...this.state, password: e.target.value });
     }
 
     render() {
@@ -53,8 +55,8 @@ class Login_Fields extends Component {
             <div className="form-container sign-in-container">
                 <form className='form' action="../Profile.js" method="post">
                     <h2 className='h2' >Sign in</h2>
-                    <input className='input' type='email' placeholder="Email" name='email' id='logEmail' onChange={this.ChangeEmail}/>
-                    <input className='input' type="password" placeholder="Password" id='logPassword' onChange={this.ChangePass}/>
+                    <input className='input' type='email' placeholder="Email" name='email' id='logEmail' onChange={this.ChangeEmail} />
+                    <input className='input' type="password" placeholder="Password" id='logPassword' onChange={this.ChangePass} />
 
                     <div className='forgot-remember-div' >
                         <input className='input' type="checkbox" id="custom-control-input" onChange={this.handleInputChange} />
--- src/components/Profiles/DeleteConfirmationDialog.js
+++ src/components/Profiles/DeleteConfirmationDialog.js
@@ -1,39 +1,67 @@
-import React, { useState } from 'react';
-import { useDialog } from 'react-st-modal';
-import fire from '../config';
-function DeleteConfirmationDialog () {
-    
-        const dialog = useDialog();
-        const [value, setValue] = useState();
-        const [pass,setpass]=useState();
-        const del=(e)=>{
-            setValue(e.target.value);
-            const user=fire.auth().currentUser;
-            const email=user.email;
-            const docs=fire.firestore().collection("User").where('email','==',email).get().then((snap)=>
-            {
-                snap.forEach((doc)=>{
-                    const password=doc.data().password;
-                    console.log(pass+","+password)
-                   if(password==pass){
-                        fire.firestore().collection('User').doc(doc.id).delete();
-                        alert('your account deleted');
-                        dialog.close(value);
-                     }
-                })
-                
+import React, { Component } from 'react';
+import fire, { firestore } from '../config';
+
+var user = fire.auth().currentUser;
+var email = user ? user.email.toString() : "";
+var password = "";
+var typedPassword = "";
+var docID = "";
+
+class DeleteConfirmationDialog extends Component {
+
+    componentDidMount() {
+        user = fire.auth().currentUser;
+        email = fire.auth().currentUser.email;
+        //console.log(email)
+        firestore.collection("User").where("email", "==", email)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    password = doc.data().password;
+                    docID = doc.id;
+                });
             })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
+    }
+
+    del = (e) => {
+        e.preventDefault();
+        if (password === typedPassword) {
+            user.delete().then(function () {
+                console.log("Auth deleted")
+            }).catch(function (error) {
+                console.log(error)
+            }).then(function () {
+                firestore.collection("User").doc(docID).delete().then(() => {
+                    console.error("Account successfully deleted");
+                }).then(function () {
+                    window.location.reload(false);
+                }).catch((error) => {
+                    console.error("Error removing document: ", error);
+                })
+            })
+        } else {
+            alert("Password is NOT correct")
         }
-        return(
-            <center className='delete-Dialog'>
-                <p style={{margin:'3px'}}>For your security, please re-enter your password to continue</p>
+    }
+
+    handleInputChange = (e) => {
+        typedPassword = e.target.value;
+    }
+
+    render() {
+        return (
+            <center className='delete-Dialog' >
+                <p style={{ margin: '3px' }}>For your security, please re-enter your password to continue</p>
                 <form>
-                    <input type='password' placeholder='Password' className='inputForDelete' onChange={(e)=>{setpass(e.target.pass)}}/>
-                    <br/>
-                    <button className='continue' onClick={del}>continue</button>
+                    <input type='password' placeholder='Password' className='inputForDelete' onChange={this.handleInputChange} />
+                    <br />
+                    <button className='continue' onClick={this.del}>continue</button>
                 </form>
             </center>
         );
-    
+    }
 }
 export default DeleteConfirmationDialog;
